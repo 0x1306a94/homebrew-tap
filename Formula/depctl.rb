c1b8907a9fac09996f3e0ecc647f27a357bddeb5
0x1306a94 <0x1306a94@gmail.com>
2026-02-18 20:58:53 +0800

    Update version for depctl

--- Formula/depctl.rb
+++ Formula/depctl.rb
@@ -2,13 +2,13 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Depctl < Formula
-  version "1.4.5"
+  version "1.4.6"
   desc "A Rust implementation of dependency management tool similar to depsync"
   homepage "https://github.com/0x1306a94/depctl"
   url "https://github.com/0x1306a94/depctl/releases/download/v#{Depctl.version}/depctl-mac-universal.zip"
   # TODO: Update sha256 after first release is created
   # Run: shasum -a 256 <downloaded_file.zip>
-  sha256 "9932ec521c78340584c8c7fac6d779d472b9086fd9cb6825a75252134b210ca8"
+  sha256 "7c5414c28d40793afae925b005649839fcef6828784bf80fb59dcd2fa056fd75"
 
   def install
     bin.install "depctl"
